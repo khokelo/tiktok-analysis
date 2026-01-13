39d4aec937ebd139cd38c1e329f91cef6a86cd1d
Usamah azzikri <163979356+usamahazzikri@users.noreply.github.com>
2026-01-14 03:20:08 +0700

    Force load routes file explicitly + remove RegisterRoutes middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,7 +5,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -19,3 +19,10 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
+// Force load routes explicitly after app creation
+if (file_exists(__DIR__.'/../routes/web.php')) {
+    require __DIR__.'/../routes/web.php';
+}
+
+return $app;
