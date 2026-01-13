dc8776e2eb2155cacd00e04d061a5d3da19a2bca
Usamah azzikri <163979356+usamahazzikri@users.noreply.github.com>
2026-01-14 03:11:47 +0700

    Add inline test route in bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,20 +4,27 @@ use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        // Temporarily disabled - causing issues
-        // $middleware->append(EnsureDatabaseMigrated::class);
         $middleware->alias([
             'admin' => AdminMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->create();
+
+// Inline test route
+Route::get('/test-inline', function () {
+    return response()->json(['route' => 'works', 'from' => 'bootstrap']);
+});
+
+return $app;
