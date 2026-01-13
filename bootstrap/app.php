6714ccc4ff10efa20b76ddfd51596eb926915d63
Usamah azzikri <163979356+usamahazzikri@users.noreply.github.com>
2026-01-14 03:09:30 +0700

    Remove unlink calls from bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,14 +1,9 @@
 <?php
 
 use App\Http\Middleware\AdminMiddleware;
-use App\Http\Middleware\EnsureDatabaseMigrated;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-
-// Ensure cache is clear for route changes
-@unlink(base_path('bootstrap/cache/routes-v7.php'));
-@unlink(base_path('bootstrap/cache/config.php'));
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
